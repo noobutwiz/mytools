2f81a1c5a238465930f065b0e2bef573f355ab1e
noobutwiz <christian.cadelina031@gmail.com>
2024-06-17 21:37:15 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+// var tweet = prompt("Compose you tweet");
+// var tweetCount = tweet.length;
+// alert("You have written " + tweetCount + " " + "characters, you have " + (140 - tweetCount) + " " + "characters remaining");
+
+// You have written 182 characters, you have -42 characters left.
+
+// var name = "Angela";
+// name.slice(0,3);
+
+// var tweet = prompt("Compose you tweet");
+// var tweetUnder140 = tweet.slice(0,140);
+// alert(tweetUnder140);
+
+var name = prompt("What is your name?");
+var firstLetter = name.slice(0, 1);
+var firstLetter = firstLetter.toUpperCase();
+var theRestOfTheLetter = name.slice(1, name.length);
+var theRestOfTheLetter = theRestOfTheLetter.toLowerCase();
+var capitalizedName = firstLetter + theRestOfTheLetter;
+alert("Hi " + capitalizedName + " Welcome Back!");
