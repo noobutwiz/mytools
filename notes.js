4484a5fb61865c5df3a4f31d8707b7c49cf64a27
noobutwiz <christian.cadelina031@gmail.com>
2024-06-21 11:21:55 +0800

    update

--- notes.js
+++ notes.js
@@ -11,14 +11,38 @@
 // var tweetUnder140 = tweet.slice(0,140);
 // alert(tweetUnder140);
 
-var name = prompt("What is your name?");
-var firstLetter = name.slice(0, 1);
-var firstLetter = firstLetter.toUpperCase();
-var theRestOfTheLetter = name.slice(1, name.length);
-var theRestOfTheLetter = theRestOfTheLetter.toLowerCase();
-var capitalizedName = firstLetter + theRestOfTheLetter;
-alert("Hi " + capitalizedName + " Welcome Back!");
+// var name = prompt("What is your name?");
+// var firstLetter = name.slice(0, 1);
+// var firstLetter = firstLetter.toUpperCase();
+// var theRestOfTheLetter = name.slice(1, name.length);
+// var theRestOfTheLetter = theRestOfTheLetter.toLowerCase();
+// var capitalizedName = firstLetter + theRestOfTheLetter;
+// alert("Hi " + capitalizedName + " Welcome Back!");
 
-var dogAge = prompt("What is your dog age?");
-humanAge = (dogAge - 2) * 4 + 21;
-alert("Your dog age is " + humanAge + " years old in human years.");
+// var dogAge = prompt("What is your dog age?");
+// humanAge = (dogAge - 2) * 4 + 21;
+// alert("Your dog age is " + humanAge + " years old in human years.");
+
+function getMilk() {
+  alert("leaveHouse");
+  alert("moveRight");
+  alert("moveRight");
+  alert("moveUp");
+  alert("moveUp");
+  alert("moveUp");
+  alert("moveUp");
+  alert("moveRight");
+  alert("moveRight");
+  alert("buyMilk");
+  alert("moveLeft");
+  alert("moveLeft");
+  alert("moveDown");
+  alert("moveDown");
+  alert("moveDown");
+  alert("moveDown");
+  alert("moveLeft");
+  alert("moveLeft");
+  alert("enterHouse");
+}
+
+getMilk();
